8dc211ca20d4b8ed0529cd4e774d5bf2b5429b7e
Jose Vizcaya <jose.vizcaya@ingeint.com>
2023-12-12 16:55:20 -0400

    [FEATURE] Etiqueta para productos en orden de fabricación.
    Se creó un reporte en la orden de fabricación para imprimir etiquetas
    (Con un formato especial para la impresora Zebra) con dimensiones 57mm x
    32mm, el cual contiene el nombre del producto a ser fabricado, la
    cantidad, su lote (Este aparece solo si se le fue asignado con
    anterioridad) y el código de barras de este mismo.

--- __manifest__.py
+++ __manifest__.py
@@ -7,13 +7,18 @@
     "author": "Ingeint",
     "category": "Stock/Barcode",
     "version": "14.0.0",
-    "depends": ["stock_barcode", "web"],
+    "depends": ["stock_barcode", "mrp", "web"],
     "data": [
         # "security/ir.model.access.csv",
+        "data/report_paperformat.xml",
+
         "views/templates.xml",
         "views/product_views.xml",
         "views/stock_location_views.xml",
         "views/stock_production_lot_views.xml",
+
+        "report/product_zebra_report_template.xml",
+        "report/amanecer_stock_barcode_report.xml",
     ],
     "qweb": [
         "static/src/xml/qweb_templates.xml",
